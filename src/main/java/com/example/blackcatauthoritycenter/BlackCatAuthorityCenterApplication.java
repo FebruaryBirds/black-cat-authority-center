6d4688d55041f304fb9c817e9dfec11071661705
‘FebruaryBirds’ <1505509862@qq.com>
2022-11-22 10:47:10 +0800

    【init】

---
+++ src/main/java/com/example/blackcatauthoritycenter/BlackCatAuthorityCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackcatauthoritycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackCatAuthorityCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackCatAuthorityCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blackcatauthoritycenter/BlackCatAuthorityCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackcatauthoritycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackCatAuthorityCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
